2cd7cd70ac4a1b981c62a1adc1239e941c1eedfe
John Eldrin Tolentino <115896724+jeldrint@users.noreply.github.com>
2024-05-15 00:53:47 +0800

    minor update
    * now using the formatted date using luxon (as a virtual schema property, again, finally!)

--- models/post.js
+++ models/post.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const { DateTime } = require('luxon');
 
 const PostSchema = new Schema({
     title: {type: String, required: true, maxLength: 500},
@@ -7,6 +8,14 @@ const PostSchema = new Schema({
     body: {type: String, required: true, maxLength: 50000},
     userId: {type: Schema.Types.ObjectId, ref: 'Users'},
     comments: {type: Schema.Types.ObjectId, ref: 'Comments'}
+})
+
+PostSchema.virtual('date_formatted').get(function () {
+    if(DateTime.fromJSDate(this.timestamp).isValid){
+        return DateTime.fromJSDate(this.timestamp).toLocaleString(DateTime.DATETIME_FULL)
+    }else{
+        return 'N/A'
+    }
 })
 
 module.exports = mongoose.model('Posts', PostSchema)
