bcd26640bb8f87ed6982023155fbb04f9fe62d7c
John Eldrin Tolentino <115896724+jeldrint@users.noreply.github.com>
2024-05-30 18:01:44 +0800

    successfully logged-in on server side

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const asyncHandler = require('express-async-handler');
 const Post = require('../models/post')
 
+const passport = require('passport')
+const User = require('../models/user')
 
+//MAIN PAGE DISPLAY FOR PUBLIC / NOT LOGGED ACCOUNTS
 router.get('/', asyncHandler(async (req,res) => {
     const posts = await Post.find().populate('userId').populate('userIdUpdated').exec();
     res.json({
@@ -12,11 +15,17 @@ router.get('/', asyncHandler(async (req,res) => {
     })
 }))
 
-
-router.post('/login', asyncHandler(async (req,res) =>{
-/*     res.json({
-        
-    })
- */}))
+//LOG-IN
+router.post('/log-in',
+    passport.authenticate('local', {
+        failureRedirect: '/techy-blog/log-in',
+    }), (req,res) => {
+        const user = new User({
+            _id: req.user._id,
+            user_name: req.user.user_name
+        })
+        res.redirect(user.url);
+    }
+)
 
 module.exports = router;
