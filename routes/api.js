7e261c96c25ba836173632c246d64077d45c61c4
John Eldrin Tolentino <115896724+jeldrint@users.noreply.github.com>
2024-06-06 23:22:40 +0800

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,12 @@ const router = express.Router();
 const asyncHandler = require('express-async-handler');
 const Post = require('../models/post')
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
+
+const User = require('../models/user')
+require('dotenv').config();
+const {body, validationResult} = require('express-validator');
+
 
 //MAIN PAGE DISPLAY FOR PUBLIC / NOT LOGGED ACCOUNTS
 router.get('/', asyncHandler(async (req,res) => {
@@ -51,6 +57,63 @@ router.get('/log-out',(req,res,next)=>{
         message: 'Logged out successfully'
     })
 })
+
+// FOR SIGN UP
+router.post('/sign-up',[
+    body('firstname')
+        .trim().isLength({min: 1}).escape().withMessage('First name is empty')
+        .matches(/^[a-zA-Z ]*$/)
+        .withMessage('Name must consist of alphabet letters only'),
+    body('lastname')
+        .trim().isLength({min: 1}).escape().withMessage('Family name is empty')
+        .matches(/^[a-zA-Z ]*$/)
+        .withMessage('Name must consist of alphabet letters only'),
+    body('username')
+        .trim().isLength({min: 7}).escape()
+        .withMessage('Username must be 7 characters long')
+        .custom(async value => {
+            const user = await User.findOne({user_name: value}).exec()
+            if (user){
+                throw new Error('Username already exists')
+            }
+        }),
+    body('password')
+        .trim().isLength({min: 8}).escape()
+        .withMessage('Password must be 8 characters long'),
+    body('confirm-password')
+        .custom((value, {req}) => value === req.body.password)
+        .withMessage('Password does not match'),
+
+    asyncHandler(async (req,res,next) =>{
+        const errors= validationResult(req);
+
+        const user = new User({
+            first_name: req.body.firstname,
+            family_name: req.body.lastname,
+            user_name: req.body.username,
+        })
+
+        if(!errors.isEmpty()){
+            return res.json({
+                errors: errors.array()
+            })
+        }else{
+            req.body.admincode === process.env.ADMIN_CODE ? user.isAdmin = true : user.isAdmin = false;
+    
+            bcrypt.hash(req.body.password, 10, async (err, hashedPw) => {
+                if(err){
+                    return err
+                }else{
+                    user.password = hashedPw;
+                    await user.save();
+                    res.json({
+                        successMessage: 'Signed up sucessfully!'
+                    })
+                }
+            })
+        }
+    })    
+])
 
 
 module.exports = router;
